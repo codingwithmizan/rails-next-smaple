1db8ec22b907af40ea465b7674f2458f9cd7caed
mizan <mizan@ifarmer.asia>
2025-03-08 22:09:18 +0600

    add dockerfile

--- frontend/src/app/(auth)/login/page.tsx
+++ frontend/src/app/(auth)/login/page.tsx
@@ -3,7 +3,7 @@ import { LoginForm } from "@/components/auth";
 const Login = () => {
   return (
     <div className="max-w-4xl mx-auto mt-10 px-10">
-      <h2 className="mb-4 text-2xl">User Login</h2>
+      <h2 className="mb-4 text-2xl">User Login55</h2>
       <LoginForm />
     </div>
   );
